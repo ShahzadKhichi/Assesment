a336679313905b57fbe77901f72937a27770eb06
shahzadkhichi <shahzadkhichi996@gmail.com>
2026-07-27 21:21:08 +0500

    hotfix:cors added

--- backend/app/config/settings.py
+++ backend/app/config/settings.py
@@ -165,25 +165,25 @@ REST_FRAMEWORK = {
 # CORS
 # ---------------------------------------------------------------------------
 # Allow specific frontend origins
-CORS_ALLOW_ALL_ORIGINS: bool = False
+CORS_ALLOW_ALL_ORIGINS: bool = True
 CORS_ALLOWED_ORIGINS: List[str] = [
-    'https://tripplannerfrontend.vercel.app',
-    'http://localhost:3000',
+    'https://tripplannerfrontend.vercel.app/',
+    'http://localhost:3000/',
 ]
 CORS_ALLOW_CREDENTIALS: bool = True
-CORS_EXPOSE_HEADERS: List[str] = ['Content-Type', 'X-CSRFToken']
-CORS_ALLOW_METHODS: List[str] = ['DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT']
-CORS_ALLOW_HEADERS: List[str] = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-]
+# CORS_EXPOSE_HEADERS: List[str] = ['Content-Type', 'X-CSRFToken']
+# CORS_ALLOW_METHODS: List[str] = ['DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT']
+# CORS_ALLOW_HEADERS: List[str] = [
+#     'accept',
+#     'accept-encoding',
+#     'authorization',
+#     'content-type',
+#     'dnt',
+#     'origin',
+#     'user-agent',
+#     'x-csrftoken',
+#     'x-requested-with',
+# ]
 
 # CSRF Configuration for cross-origin requests
 CSRF_TRUSTED_ORIGINS: List[str] = [
